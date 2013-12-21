08190dc11fa212884deb4b291364d3c787646ecf
AndSelm <and.selm@gmail.com>
2013-12-21 17:32:59 +0100

    beginning angular

--- source/frontend-main.ts
+++ source/frontend-main.ts
@@ -1,6 +1,7 @@
 
 /// <reference path="../declarations/node.d.ts" />
 /// <reference path="../declarations/node-webkit.d.ts" />
+/// <reference path="../declarations/angular.d.ts" />
 
 
 import gui = require('nw.gui');
@@ -22,4 +23,19 @@ function doIt() {
 	Frontend.executeAction("hotkey", { hotkey: "ctrl+p" }).fail(function (e) {
 		console.log(e);
 	});
+}
+
+module CP {
+
+	//angular.module("CP.directives", []);
+	//angular.module("CP.filters", []);
+	 
+	angular.module("CP", [/*"CP.services", "CP.filters", "CP.directives"*/]).
+		config( () => {
+	
+		}).
+		controller("Main", $scope => {
+			$scope.commands = ["Start", "Foobar"];
+
+		});
 }
